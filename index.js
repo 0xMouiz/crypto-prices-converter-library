8d4147e93c9dd2426329c6e77a7cb62c77f8b6c5
Mouizeddine <mouizm165@gmail.com>
2023-06-22 20:59:12 +0100

    convertCurrency added!

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const getCryptoPrice = async (cryptoName, quantity, currencyCode) => {
+const convertCrypto = async (cryptoName, quantity, currencyCode) => {
   try {
     const id = cryptoName.toLowerCase();
 
@@ -30,4 +30,38 @@ const getCryptoPrice = async (cryptoName, quantity, currencyCode) => {
   }
 };
 
-module.exports = getCryptoPrice;
+const convertCurrency = async (currencyCode, quantity, cryptoName) => {
+  try {
+    const id = cryptoName.toLowerCase();
+
+    if (quantity < 0) {
+      throw new Error("Quantity must be 0 or more!");
+    }
+
+    const res = await fetch(
+      `https://api.coingecko.com/api/v3/coins/${id}?localization=false&tickers=false&community_data=false&developer_data=false`
+    );
+
+    if (!res.ok) throw new Error("Crypto Not Available");
+
+    const data = await res.json();
+
+    if (!data.market_data.current_price.hasOwnProperty(currencyCode)) {
+      throw new Error("Currency Not Available!");
+    }
+
+    const cryptoPrice = data.market_data.current_price[currencyCode];
+
+    const cryptoQuantity = quantity / cryptoPrice;
+
+    // console.log(cryptoQuantity);
+
+    return cryptoQuantity;
+  } catch (err) {
+    if (err instanceof Error) {
+      console.log(err);
+    }
+  }
+};
+
+module.exports = { convertCurrency, convertCrypto };
